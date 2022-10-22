e4a4767175c573201126c965dd132d2b559fb825
vinics <vinics.dr@gmail.com>
2022-10-22 08:19:11 -0300

    fix: Correct hover text color fot primary button component

--- src/components/PrimaryButton.tsx
+++ src/components/PrimaryButton.tsx
@@ -5,7 +5,7 @@ interface IPrimaryButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
 export function PrimaryButton(props: IPrimaryButtonProps) {
   return (
     <button
-      className="text-gray-40 h-12 bg-turq-300 text-gray-050 rounded hover:bg-turq-200"
+      className="text-white h-12 bg-turq-300 text-gray-050 rounded hover:bg-turq-200 hover:text-gray-400"
       {...props}>
         {props.children}
     </button>
